86a88a7de9bca7759f39710b3b14b04f30bc6ab6
KKhang <127233909+SevKan10@users.noreply.github.com>
2025-02-16 13:19:53 +0700

    ADD HOW TO USE BOARD FILE

---
+++ How_Use_Board/How_Use_Board.ino
@@ -0,0 +1,9 @@
+INSTALL ESP 8266
+https://arduino.esp8266.com/stable/package_esp8266com_index.json 
+
+INSTALL ESP 32
+https://dl.espressif.com/dl/package_esp32_index.json
+
+INSTALL ADRUINO NANO V3 BLU
+https://raw.githubusercontent.com/dbuezas/lgt8fx/master/package_lgt8fx_index.json
+  THEN: Search on board manger "lgt" click install "LGT8fx Boards" 
